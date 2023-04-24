d13e9031c6d4075e1ac480e3bf98d9895ba6600d
Matteo Moretti <hello@matteomoretti.com>
2023-04-24 17:13:49 +0200

    titolo fixes on sm

--- algheroXdesign/components/titolo.vue
+++ algheroXdesign/components/titolo.vue
@@ -1,6 +1,6 @@
 <template>
       <div class="grid grid-cols-12 gap-x-6 ">
-            <div class="px-5 col-span-10 col-start-1 sm:col-start-1 sm:px-0 py-12">
+            <div class="px-5 col-span-12 col-start-1 sm:col-start-1 sm:px-0 py-12">
                 <h1>{{ props.titoloPagina }}</h1>
             </div>
         </div>
