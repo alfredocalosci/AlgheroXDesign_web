b6e90b7805831c6b92beb68834c74912560d7804
Matteo Moretti <hello@matteomoretti.com>
2023-04-24 17:47:45 +0200

    testi corretti piano di studi

--- algheroXdesign/pages/piano-studi.vue
+++ algheroXdesign/pages/piano-studi.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="container mx-auto" id="pds">
      <titolo titoloPagina="il piano di studi"></titolo>
-        <div class="grid grid-cols-12 gap-x-6">
+        <div class="grid grid-cols-12 gap-x-6 mb-12">
             <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
                 <p>
-                    La didattica è organizzata in laboratori progettuali e insegnamenti teorici, Maecenas faucibus mollis interdum. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla. Maecenas sed diam eget risus varius blandit sit amet non magna.
-                    Nullam id dolor id nibh ultricies vehicula ut id elit. Donec id elit non mi porta gravida at eget metus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.
+                    Il progetto didattico si ispira a un modello interdisciplinare, a cavallo tra teoria e pratica, in cui le attività laborartoriali e progettuali si alternano con gli insegnamenti teorici. 
+                    A completare la formazione sono sono i workshop interclase: corsi intensivi della durata di una settimana, in cui docenti e profesisonisti di fama iternazionale guidano gli studenti in vere e proprie esperieze progettuali.
+                
                     </p>
             </div>
         </div>
@@ -21,26 +22,90 @@
                 </p>
             </div>
             <div class="col-span-12 col-start-1 px-4 sm:px-0 xl:col-span-2 lg:col-span-3 md:col-span-3 col-span-12 col-start-1 py-7">
-                    <ul>1 semestre
-                    <li>Lab di Design per il pianeta</li>
+                    <ul>1 semestre - I anno
                     <li>Immagini I - Disegno</li>
                     <li>Immagini I - Fotografia</li>
-                    <li> Geometria</li>
+                    <li>Geometria</li>
+                    <li>Storia del design</li>
+                    <li>Lab. di Design per il pianeta</li>
+                    <li>Filosofia dell'arte</li>
                     </ul>
                     
                     <ul>
                     <li class="py-[15.75px]">workshop interclasse</li>
                     </ul>
                     
-                    <ul>2 semestre
-                    <li>Lab di Design per il pianeta</li>
-                    <li>Immagini I - Disegno</li>
-                    <li>Immagini I - Fotografia</li>
-                    <li> Geometria</li>
+                    <ul>2 semestre - I anno
+                    <li>Lab. di Design e luoghi</li>
+                    <li>Antropologia culturale</li>
+                    <li>Culture del design</li>
+                    
                     </ul>
             </div>
         </div>
 
+        <div class="grid grid-cols-12 gap-x-6" id="desc">
+            <div class="col-span-12 col-start-1 px-4 sm:px-0 py-7 lg:col-span-6 lg:col-start-3 md:col-span-8 ">
+                    <h6>Secondo anno</h6>
+                <p>
+                    Nel primo semestre si concentrano insegnamenti legati alle pratiche comunicative dell'attuale scenario dei media:
+il design della comunicazione nel contesto dei media digitali (multimedia, animazione); l'info-design.     
+                </p>
+                <p>
+                    Nel secondo semestre, il compito di confrontarsi con la progetta- zione di prodotto e dei beni materiali è assegnato al laboratorio integrato di Design della cultura materiale, nel quale si guarderà con particolare attenzione alle nuove spinte verso l'autocostruzione, il nuovo artigianato, la fabbricazione digitale. </p>
+            </div>
+            <div class="col-span-12 col-start-1 px-4 sm:px-0 xl:col-span-2 lg:col-span-3 md:col-span-3 col-span-12 col-start-1 py-7">
+                    <ul>3 semestre - II anno
+                    <li>Lab. di Digital design</li>
+                    <li>Lab. di Info design</li>
+                    <li>Immagini II - Linguaggi grafici</li>
+                    <li>Teoria dei linguaggi audivisi</li>
+                    <li>Attività a scelta</li>
+                    </ul>
+                    
+                    <ul>
+                    <li class="py-[15.75px]">workshop interclasse</li>
+                    </ul>
+                    
+                    <ul>4 semestre - II anno
+                    <li>Lab. di Design della cultura materiale</li>
+                    <li>Tecnologia dei processi di lavorazione</li>
+                    <li>Modelli e tecnologie dei materiali per il design</li>
+                    <li>Marketing</li>
+                    </ul>
+            </div>
+        </div>
+
+        <div class="grid grid-cols-12 gap-x-6" id="desc">
+            <div class="col-span-12 col-start-1 px-4 sm:px-0 py-7 lg:col-span-6 lg:col-start-3 md:col-span-8 ">
+                    <h6>Terzo anno</h6>
+                <p>
+                    Nel laboratorio di Exhibit design del primo semestre gli studenti si confronteranno con la dimensione degli allestimenti e del progetto di interni, dove il design della comunicazione e il progetto dello spazio si intrecciano, incontrando la dimensione emergente del design delle esperienze.
+Al corso Design per la società è assegnato il compito di evidenziare gli scenari di relazioni e interazione che legano le discipline del design con la progettazione di processi e servizi.
+                </p>
+                <p>
+                    Il secondo semestre è dedicato alle attività di sintesi del processo di apprendimento: elaborazione del portfolio personale, esami a scelta, perfezionamento della lingua inglese, preparazione dell'elaborato di fine carriera, che in linea con la tradizione del DADU, si innesteranno su un articolato programma di tirocini professionalizzanti, dentro e fuori dalla Sardegna, con una predi- lezione per le esperienze all'estero.    
+                 </p>
+            </div>
+            <div class="col-span-12 col-start-1 px-4 sm:px-0 xl:col-span-2 lg:col-span-3 md:col-span-3 col-span-12 col-start-1 py-7">
+                    <ul>5 semestre - III anno
+                    <li>Lab. di Exhibit design</li>
+                    <li>Design per la società</li>
+                    <li>Attività a scelta</li>
+                    </ul>
+                    
+                    <ul>
+                    <li class="py-[15.75px]">workshop interclasse</li>
+                    </ul>
+                    
+                    <ul>2 semestre - III anno
+                    <li>Tirocinio</li>
+                    <li>Attività a scelta</li>
+                    <li>Lingua inglese</li>
+                    <li>Prova finale</li>
+                    </ul>
+            </div>
+        </div>
        
 
         <saperne></saperne>
@@ -83,6 +148,16 @@
     }
 
 #pds{
+    p{
+        @apply text-[25px];
+        @apply leading-[125%];
+        @apply font-[400];
+        @apply tracking-[-.02em];
+        @apply lg:text-[25px];
+        @apply lg:leading-[125%];
+        @apply lg:font-[400];
+        @apply lg:tracking-[-.02em];
+    }
     #desc{
         p{
         @apply normal-case;
@@ -98,9 +173,9 @@
         @apply leading-[28px];       
     }
     ul{
-        @apply text-[14px];
+        @apply text-[12px];
         @apply uppercase;
-        @apply font-[500];
+        @apply font-[400];
         @apply leading-[28px];
         li{
             @apply normal-case;
