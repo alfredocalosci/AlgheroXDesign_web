5d39c10cb8ee10c1ff74f071e42cb8a02997efcf
Matteo Moretti <hello@matteomoretti.com>
2023-04-25 19:31:42 +0200

    footer fixed

--- algheroXdesign/components/footer-global.vue
+++ algheroXdesign/components/footer-global.vue
@@ -1,50 +1,86 @@
 <template>
-     <div class="border-t border-black mb-8 px-4">
-        <div class="container md:mx-auto mt-8">
-            <div class="grid md:grid-cols-4 gap-6 mt-4">
+     <div class="border-t border-black mb-8 px-4 mt-[10rem] sm:mt-0">
+        <div class="container sm:mx-auto sm:mt-0 ">
+            <div class="grid grid-cols-4 gap-6 mt-4 mb-2 sm:mb-0">
+                <div>
                 <img src="~/assets/img/logo_architecture_at_alghero.jpg" alt="architettura ad alghero" class="h-16 mt-2">
-                <img src="~/assets/img/sigillo.png" alt="UNISS" class="h-20 ">
+                 </div>
+       
+                 <div class="col-start-4">
+                    <img src="~/assets/img/sigillo.png" alt="UNISS"  class="h-16 mt-2 hidden sm:block">
+                 </div>
             </div>
 
-            <div class="grid md:grid-cols-4 gap-6 mt-4 ">
+            <div class="grid sm:grid-cols-4 gap-6">
                 <div>
-                    <p class="text-sm mt-4">
-                        <span class="font-medium">Dipartimento di Architettura, Design e Urbanistica</span><br/>
+                    <p class="text-sm font-[500]">
+                       Dipartimento di Architettura, Design e Urbanistica
+                    </p>
+                    <p class="text-sm">
+                            Università degli Studi di Sassari
+                    </p>
+                    <p class="text-sm">
+                            Palazzo del Pou Salit - Piazza Duomo, 6 
+                    </p>
+                    <p class="text-sm">
+                        07041 Alghero (SS), Italia
+                    </p>
+                   
+                </div>
 
-                        <span class="block my-4">
-                            Università degli Studi di Sassari<br/>
-                            Palazzo del Pou Salit – Piazza Duomo, 6 <br/>
-                        </span>
+               
 
-                        07041 Alghero<br/>
-                        Tel.: +39 079 972
-
+                <div>
+                    <p class="text-sm font-[500]">
+                        Segreteria studenti
+                    </p>
+                    <p class="text-sm">
+                        Telefono +39 079 9720451
+                    </p>
+                    <p class="text-sm">
+                        Mobile +39 333 3300428
+                    </p>
+                    <p class="text-sm">
+                        Email <a href="mailto:a.catogno@uniss.it">a.catogno@uniss.it</a>
+                    </p>
+                    
+                    
+                </div>
+                <div>
+                    <p class="text-sm font-[500]">
+                        Orari al pubblico:
+                    </p>
+                    <p class="text-sm">
+                        Lunedì, Mercoledì, Venerdì: 09:00-13:00
+                    </p>
+                    <p class="text-sm">
+                        Martedì, Giovedì: 15:00 -17:00
+                    </p>
+                    <p class="text-sm">
+                            Palazzo del Pou Salit - Piazza Duomo, 6 
+                    </p>
+                    <p class="text-sm">
+                        07041 Alghero (SS), Italia
                     </p>
                 </div>
 
                 <div>
-                    <p class="text-sm mt-4">
-                        <span class="font-medium">Università degli Studi di Sassari</span><br/>
-                      
+                    <p class="text-sm font-[500] block sm:hidden">
+                        <img src="~/assets/img/sigillo.png" alt="UNISS"  class="h-16 mt-2">
+                    </p>
+                    <p class="text-sm font-[500]">
+                        Università degli Studi di Sassari
+                    </p>
+                    <p class="text-sm">
+                        Piazza Università, 21
+                    </p>
+                    <p class="text-sm">
+                    07100 Sassari (SS), Italia
+                    </p>
+                    <p class="text-sm">
+                        Telefono +39 079 228211/8816
                     </p>
                 </div>
-
-                <div>
-                    <p class="text-sm mt-4">
-                        <span class="font-medium">Segreteria</span><br/>
-                        segreteriaxdesign@uniss.it
-                    </p>
-                </div>
-
-
-                <div>
-                    <p class="text-sm mt-4">
-                        <span class="font-medium">Segreteria</span><br/>
-                        segreteriaxdesign@uniss.it
-                    </p>
-                </div>
-
-
             </div>
         </div>
     </div>
@@ -52,5 +88,7 @@
 
 <style lang="scss" scoped>
 
-
+p{
+    letter-spacing: -0.01em;
+}
 </style>
