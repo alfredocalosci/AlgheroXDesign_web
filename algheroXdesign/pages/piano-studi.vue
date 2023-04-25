6ebd286e72d2457a7198e5393d8b4dab01b34e99
Matteo Moretti <hello@matteomoretti.com>
2023-04-25 18:26:16 +0200

    greysh back full-w piano di studi

--- algheroXdesign/pages/piano-studi.vue
+++ algheroXdesign/pages/piano-studi.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div class="container mx-auto" id="pdsss">
+    <div class="container mx-auto pds">
         <titolo titoloPagina="il piano di studi"></titolo>
         <div class="grid grid-cols-12 gap-x-6 mb-12">
             <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
@@ -13,9 +13,9 @@
         </div>
     </div>
 
-    <section class="bg-slate-100">
-        <div class="container mx-auto" >
-            <div class="grid grid-cols-12 gap-x-6 bg-slate-100 desc">
+    <section class="bg-slate-100 pds">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-12 gap-x-6 desc">
             <div class="col-span-12 col-start-1 px-4 sm:px-0 py-7 lg:col-span-6 lg:col-start-3 md:col-span-8 ">
                     <h6>Primo anno</h6>
                 <p>
@@ -52,52 +52,9 @@
     </section>
 
 
-    <div class="container mx-auto" id="pds">
-     <titolo titoloPagina="il piano di studi"></titolo>
-        <div class="grid grid-cols-12 gap-x-6 mb-12">
-            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
-                <p>
-                    Il progetto didattico si ispira a un modello interdisciplinare, a cavallo tra teoria e pratica, in cui le attività laborartoriali e progettuali si alternano con gli insegnamenti teorici. 
-                    A completare la formazione sono sono i workshop interclase: corsi intensivi della durata di una settimana, in cui docenti e profesisonisti di fama iternazionale guidano gli studenti in vere e proprie esperieze progettuali.
-                
-                    </p>
-            </div>
-        </div>
-        <div class="grid grid-cols-12 gap-x-6 bg-slate-100 desc">
-            <div class="col-span-12 col-start-1 px-4 sm:px-0 py-7 lg:col-span-6 lg:col-start-3 md:col-span-8 ">
-                    <h6>Primo anno</h6>
-                <p>
-                    Al primo semestre del primo anno di studi il laboratorio di Design per il pianeta pone l'accento sugli emergenti problemi alla scala planetaria, sottolineando la necessità di intervenire a livello di sistema, e indirizzando sin da subito il corso di studi verso una linea di piena consapevolezza ambientale.
-                </p>
-                <p>
-                    Nel secondo semestre, con il laboratorio integrato di Design e Luoghi l'enfasi si trasferisce sulla dimensione territoriale, evidenziando la necessità di un confronto costante dell'uomo con il suo intorno e – per i progettisti – l'importanza di acuire la propria capacità di ascolto. 
-                    Al corso di Culture del Design è assegnato il compito di sensibilizzare gli studenti sugli aspetti me- todologici della progettazione e sull'insostituibile ruolo di media- zione culturale che il design è chiamato a svolgere per la società.
-                </p>
-            </div>
-            <div class="col-span-12 col-start-1 px-4 sm:px-0 xl:col-span-2 lg:col-span-3 md:col-span-3 col-span-12 col-start-1 py-7">
-                    <ul>1 semestre - I anno
-                    <li>Immagini I - Disegno</li>
-                    <li>Immagini I - Fotografia</li>
-                    <li>Geometria</li>
-                    <li>Storia del design</li>
-                    <li>Lab. di Design per il pianeta</li>
-                    <li>Filosofia dell'arte</li>
-                    </ul>
-                    
-                    <ul>
-                    <li class="py-[15.75px]">workshop interclasse</li>
-                    </ul>
-                    
-                    <ul>2 semestre - I anno
-                    <li>Lab. di Design e luoghi</li>
-                    <li>Antropologia culturale</li>
-                    <li>Culture del design</li>
-                    
-                    </ul>
-            </div>
-        </div>
-
-        <div class="grid grid-cols-12 gap-x-6 bg-slate-200 desc">
+    <section class="bg-slate-200 pds">
+        <div class="container mx-auto">
+        <div class="grid grid-cols-12 gap-x-6 desc">
             <div class="col-span-12 col-start-1 px-4 sm:px-0 py-7 lg:col-span-6 lg:col-start-3 md:col-span-8 ">
                     <h6>Secondo anno</h6>
                 <p>
@@ -128,43 +85,49 @@ il design della comunicazione nel contesto dei media digitali (multimedia, anima
                     </ul>
             </div>
         </div>
-
-        <div class="grid grid-cols-12 gap-x-6 bg-slate-300 desc">
-            <div class="col-span-12 col-start-1 px-4 sm:px-0 py-7 lg:col-span-6 lg:col-start-3 md:col-span-8 ">
-                    <h6>Terzo anno</h6>
-                <p>
-                    Nel laboratorio di Exhibit design del primo semestre gli studenti si confronteranno con la dimensione degli allestimenti e del progetto di interni, dove il design della comunicazione e il progetto dello spazio si intrecciano, incontrando la dimensione emergente del design delle esperienze.
-Al corso Design per la società è assegnato il compito di evidenziare gli scenari di relazioni e interazione che legano le discipline del design con la progettazione di processi e servizi.
-                </p>
-                <p>
-                    Il secondo semestre è dedicato alle attività di sintesi del processo di apprendimento: elaborazione del portfolio personale, esami a scelta, perfezionamento della lingua inglese, preparazione dell'elaborato di fine carriera, che in linea con la tradizione del DADU, si innesteranno su un articolato programma di tirocini professionalizzanti, dentro e fuori dalla Sardegna, con una predi- lezione per le esperienze all'estero.    
-                 </p>
-            </div>
-            <div class="col-span-12 col-start-1 px-4 sm:px-0 xl:col-span-2 lg:col-span-3 md:col-span-3 col-span-12 col-start-1 py-7">
-                    <ul>5 semestre - III anno
-                    <li>Lab. di Exhibit design</li>
-                    <li>Design per la società</li>
-                    <li>Attività a scelta</li>
-                    </ul>
-                    
-                    <ul>
-                    <li class="py-[15.75px]">workshop interclasse</li>
-                    </ul>
-                    
-                    <ul>2 semestre - III anno
-                    <li>Tirocinio</li>
-                    <li>Attività a scelta</li>
-                    <li>Lingua inglese</li>
-                    <li>Prova finale</li>
-                    </ul>
-            </div>
-        </div>
-       
-
-        <saperne></saperne>
-
-    
     </div>
+    </section>
+
+    <section class="bg-slate-300 pds">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-12 gap-x-6 desc">
+                <div class="col-span-12 col-start-1 px-4 sm:px-0 py-7 lg:col-span-6 lg:col-start-3 md:col-span-8 ">
+                    <h6>Terzo anno</h6>
+                     <p>
+                    Nel laboratorio di Exhibit design del primo semestre gli studenti si confronteranno con la dimensione degli allestimenti e del progetto di interni, dove il design della comunicazione e il progetto dello spazio si intrecciano, incontrando la dimensione emergente del design delle esperienze.
+                    Al corso Design per la società è assegnato il compito di evidenziare gli scenari di relazioni e interazione che legano le discipline del design con la progettazione di processi e servizi.
+                    </p>
+                    <p>
+                    Il secondo semestre è dedicato alle attività di sintesi del processo di apprendimento: elaborazione del portfolio personale, esami a scelta, perfezionamento della lingua inglese, preparazione dell'elaborato di fine carriera, che in linea con la tradizione del DADU, si innesteranno su un articolato programma di tirocini professionalizzanti, dentro e fuori dalla Sardegna, con una predi- lezione per le esperienze all'estero.    
+                     </p>
+                </div>
+                <div class="col-span-12 col-start-1 px-4 sm:px-0 xl:col-span-2 lg:col-span-3 md:col-span-3 col-span-12 col-start-1 py-7">
+                        <ul>5 semestre - III anno
+                        <li>Lab. di Exhibit design</li>
+                        <li>Design per la società</li>
+                        <li>Attività a scelta</li>
+                        </ul>
+                        
+                        <ul>
+                        <li class="py-[15.75px]">workshop interclasse</li>
+                        </ul>
+                        
+                        <ul>2 semestre - III anno
+                        <li>Tirocinio</li>
+                        <li>Attività a scelta</li>
+                        <li>Lingua inglese</li>
+                        <li>Prova finale</li>
+                        </ul>
+                </div>
+            </div>
+       
+        </div>
+        
+    </section>
+    <div class="container mx-auto pds">
+        <saperne></saperne>
+    </div>
+   
 </template>
 
 <script lang="ts">
@@ -200,7 +163,7 @@ Al corso Design per la società è assegnato il compito di evidenziare gli scena
         }
     }
 
-#pds{
+.pds{
     p{
         @apply text-[25px] leading-[125%] font-[400] tracking-[-.02em] lg:text-[25px] lg:leading-[125%] lg:font-[400] lg:tracking-[-.02em];
     }
