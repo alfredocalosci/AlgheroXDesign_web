963d596dffad050e2f76fb78e243ac410882103f
Matteo Moretti <hello@matteomoretti.com>
2023-04-26 14:35:14 +0200

    home page double hero (mob/desk)

--- algheroXdesign/pages/index.vue
+++ algheroXdesign/pages/index.vue
@@ -2,7 +2,8 @@
     <div class="container mx-auto border-black">
         <!-- testata -->
         <div class="relative h-[60vh] overflow-hidden min-h-[475px] sm:h-[25vh]">
-            <img src="~assets/img/alghero.png" alt="cupola di san michele" class="object-cover">
+            <img src="~assets/img/alghero-play-desktop.jpg" alt="cupola di san michele" class="object-cover hidden sm:block">
+            <img src="~assets/img/alghero-play-mobile.jpg" alt="cupola di san michele" class="object-cover block sm:hidden">
             <h1 class="px-4 absolute top-[5vh] left-0 text-white text-[70px] leading-[101%]  tracking-[-.05em] sm:w-full sm:text-center sm:top-[45%]">15 anni di design ad Alghero</h1>
         </div>
     </div>
@@ -39,14 +40,14 @@
         <div class="grid sm:grid-cols-2 gap-6">
             <!-- colonna sinistra-->
             <div>
-               <div class="blocco mb-0 sm:mb-4">
+               <div class="blocco mb-0 sm:mb-4" id="perche">
                     <img src="/img/girl.jpg" alt="" class="w-full">
                     <h2 class="px-4 absolute top-[5vh] left-0 text-white text-[70px] leading-[90%]  tracking-[-.05em]">Perché design ad Alghero?</h2>
                      <NuxtLink to="/studiare-ad-alghero" class="button absolute bottom-4 left-4">Si può!</NuxtLink>
                      
                 </div>
 
-                <div class="blocco mb-0 sm:mb-4">
+                <div class="blocco mb-0 sm:mb-4" id="workshop">
                     <img src="@/assets/img/axd_color.svg" alt="" class="w-full object-none">
                     <div class="absolute top-8 left-4 w-[90%]">
                         <h2>Workshops</h2>
@@ -56,7 +57,54 @@
                     <a href="#" class="button absolute bottom-4 left-4 text-black border-black">Edizione 2023</a>
                 </div>
 
-                <div class="blocco mb-0 sm:mb-4">
+                <div class="blocco mb-0 sm:mb-4" id="start">
+                    <img src="/img/start.jpg" alt="" class="w-full object-fit">
+                    <div class="absolute top-8 left-4 w-[90%] text-white">
+                        <h2>Da dove si inizia?</h2>
+                    </div>
+                    <a href="https://www.architettura.aho.uniss.it/it/didattica/corso-di-laurea-design" target="_blank" class="button absolute bottom-4 left-4">Da qui</a>
+                </div>
+
+                <div class="blocco mb-0 sm:mb-4" id="2co">
+                    <img src="@/assets/img/2co.svg" alt="" class="w-full object-fit">
+                    <div class="absolute top-8 left-4 w-[90%] text-white">
+                        <h2>Comunicare la complessità, dal 2013</h2>
+                    </div>
+                    <a href="http://www.2coconference.org" target="_blank" class="button absolute bottom-4 left-4">Wow</a>
+                </div>
+                
+
+            </div>
+
+            <!-- colonna destra-->
+            <div>
+                <div class="blocco px-4 py-4 mb-4">
+                    <h2>AlgheroXDesign è la Laurea Triennale in Design, capace di attivare studi, ricerche e iniziative per contribuire al rilancio del territorio, riprendendo a formare le figure di giovani designer di prodotto, della comunicazione e dei servizi, con cui ricominciare a immaginare, comunicare e innovare la Sardegna.</h2>
+                </div>        
+                 <!-- play alghero -->
+                 <div class="blocco mb-0 sm:mb-4" id="play">
+                    <div class="grid grid-cols-2 gap-0">
+                        <img src="/img/nicole.png" alt="" class="h-full object-cover">
+                        <div class="bg-rosso relative p-4">
+                            <h2 class="w-[90%] block text-white">Play Alghero</h2>
+                            <a href="https://www.youtube.com/watch?v=RILcP5B7wbA" target="_blank" class="button absolute bottom-4 left-4">Scopri di più</a>
+                        </div>
+                    </div>
+                </div>
+
+                
+
+                <div class="blocco mb-0 sm:mb-4" id="expo">
+                    <img src="/img/design-duo.png" alt="" class="w-full object-fit">
+                    <div class="absolute top-8 left-4 w-[90%]">
+                        <h2>Animazione e Design</h2>
+                        <p>L'esperienza di ALgheroXDesign al servizion dell'expo di Milano</p>
+                    </div>
+                   
+                    <a href="#" class="button absolute bottom-4 left-4 text-black border-black">Scopri di più</a>
+                </div>
+               
+                <div class="blocco mb-0 sm:mb-4" id="design-duo">
                     <img src="/img/design-duo.png" alt="" class="w-full object-fit">
                     <div class="absolute top-8 left-4 w-[90%]">
                         <h2>Design Duo</h2>
@@ -66,34 +114,8 @@
                     <a href="#" class="button absolute bottom-4 left-4 text-black border-black">Scopri di più</a>
                 </div>
 
-                
-
-            </div>
-
-            <!-- colonna destra-->
-            <div>
-                <div class="blocco px-4 py-4 mb-4">
-                    <p class="">AlgheroXDesign è la Laurea Triennale in Design, capace di attivare studi, ricerche e iniziative per contribuire al rilancio del territorio, riprendendo a formare le figure di giovani designer di prodotto, della comunicazione e dei servizi, con cui ricominciare a immaginare, comunicare e innovare la Sardegna.</p>
-                </div>        
-
-                <div class="blocco mb-0 sm:mb-4">
-                    <img src="/img/start.jpg" alt="" class="w-full object-fit">
-                    <div class="absolute top-8 left-4 w-[90%] text-white">
-                        <h2>Da dove si inizia?</h2>
-                    </div>
-                    <a href="https://www.architettura.aho.uniss.it/it/didattica/corso-di-laurea-design" target="_blank" class="button absolute bottom-4 left-4">Da qui</a>
-                </div>
-
-                <!-- play alghero -->
-                <div class="blocco mb-0 sm:mb-4">
-                    <div class="grid grid-cols-2 gap-0">
-                        <img src="/img/nicole.png" alt="" class="h-full object-cover">
-                        <div class="bg-rosso relative p-4">
-                            <h2 class="w-[90%] block text-white">Play Alghero</h2>
-                            <a href="https://www.youtube.com/watch?v=RILcP5B7wbA" target="_blank" class="button absolute bottom-4 left-4">Scopri di più</a>
-                        </div>
-                    </div>
-                </div>
+               
+               
             </div>
         </div>
     </div>
