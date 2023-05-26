00841d5ea833fda7b8c3d09a32a08bfa25bbd219
Matteo Moretti <hello@matteomoretti.com>
2023-05-26 18:52:45 +0200

    la sede pics

--- algheroXdesign/pages/la-sede.vue
+++ algheroXdesign/pages/la-sede.vue
@@ -5,35 +5,52 @@
             <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
                
                 <h4 class="pt-4">La sede di Alghero</h4>
-                <p>La Facoltà di Architettura di Alghero, ora Dipartimento, è nata nel 2002, prima facoltà di architettura in Sardegna.</p>
-
-            </div>
-            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
-               
-                <h4 class="pt-4">Ambiente locale e qualità delle relazioni</h4>
-                <p>L'offerta formativa di AlgheroXDesign è unica nel suo genere. Coniuga di fatto la dimensione locale, punto di forza del Corso di Laurea, con la visione e la portata globale dei temi e degli insegnamenti proposti. Questi sono caratterizzati da un rapporto docente-studente di qualità, attento alle relazioni e ai bisogni, complice anche il numero limitato di posti. Questo garantisce la qualità e la sostenibilità dell'offerta durante tutto il ciclo di studi.</p>
+                <p>La Facoltà di Architettura di Alghero, ora Dipartimento, è nata nel 2002, prima facoltà di architettura in Sardegna. Sono tre le sedi in Alghero: <i>Palazzo del Pou Salit</i>,sede amministrativa, segreteria studenti e studi docenti; <i>Ex Ospedale di Santa Chiara</i>, sede didattica; <i>Ex Asilo Sella</i>, sede didattica e studi docenti.</p>
+                <p>
+                    Le sedi sono facilmente raggiungibili a piedi, situate nel centro storico di Alghero, e collegate tra di loro, dal centro storico 
+                </p>
             </div>
             <div class="col-span-12 col-start-1 sm:col-span-10 sm:col-start-2 my-12">
                 <img src="/img/233_architettura_0.jpg" alt="">
                 <p class="caption">La sede di Santa Chiara in Alghero.</p>
              </div>
-
-            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
-                <h4 class="pt-4">Sbocchi professionali </h4>
-                <p>
-                    Una volta laureati, gli studenti lavorano nel campo del visual e motion design, così come in quello del graphic design o dell'editoria multimediale, declinando le varie competenze alle esigenze e ai linguaggi della progettazione contemporanea. Studi di progettazione, istituzioni culturali, imprese creative di media e grande dimensione sono tra i principali ambiti nei quali i nostri laureati lavorano.
-                </p>
-            </div>
-            <div class="col-span-12 col-start-1 sm:col-span-10 sm:col-start-2 my-12">
+             <div class="col-span-12 col-start-1 sm:col-span-10 sm:col-start-2 my-12">
                 <img src="/img/239_architettura.jpg" alt="">
                 <p class="caption"> Biblioteca della sede di Santa Chiara - Alghero.</p>
              </div>
-            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 mb-3">
-                <h4 class="pt-4">Come raggiungere Alghero </h4>
-                <p>
-                  Di seguito una selezione di chi ha voluto raccontarci la sua esperienza tra Londra, Barcellona, e Italia.
+             
+             <div class="col-span-12 col-start-1 sm:col-span-10 sm:col-start-2 my-12">
+                <img src="/img/233_architettura.jpg" alt="">
+                <p class="caption"> Una lezione di laboratorio nella sede di Santa Chiara - Alghero.</p>
+             </div>
 
-                </p><br/>
+            
+            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
+                <h4 class="pt-4">Raggiungere Alghero </h4>
+                <p>
+                   Alghero 
+                </p>
+            </div>
+           
+            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 mb-3">
+                <h4 class="pt-4">Autobus </h4>
+                <p> Alghero è servita dalle linee del <a href="http://www.arst.sardegna.it/orari_e_autolinee/servizi_urbani/alghero.html">Trasporto regionale della Sardegna</a>.
+                  
+                </p>
+
+                <h4 class="pt-4">In aereo </h4>
+                <p> Alghero è comodamente collegata alle principali città Italiane, e con voli quotidiani da Milano, Roma e Napoli. <a href="http://www.aeroportodialghero.it">L'aeroporto Fertilia "Riviera del Corallo"</a> si trova a circa 10 km dal centro della città <a href="https://www.aeroportodialghero.it/autobus.html">collegato da diverse autolinee</a>.
+                  
+                </p>
+                
+                <h4 class="pt-4">Via mare </h4>
+                <p> Il porto passeggeri più vicino ad Alghero è a Porto Torres (circa. 40 km). Il porto di Porto Torres è collegato a Barcellona,  Genova, Civitavecchia, Marsiglia, Propriano  e Ajaccio (Corsica). Alcuni itinerari sono solo stagionali.Per ulteriori informazioni sulla frequenza, orari e tariffe si prega di visitare i siti delle compagnie di navigazione.
+                  
+                </p>
+               
+                
+
+            
             </div>
             
         </div>
