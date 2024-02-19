f97178a28dd93d3e569018c97c33f99cd1cc11e5
Matteo Moretti <hello@matteomoretti.com>
2024-02-19 15:03:32 +0100

    workshop update

--- algheroXdesign/pages/workshop.vue
+++ algheroXdesign/pages/workshop.vue
@@ -46,38 +46,10 @@
                     combinano, su tematiche espresse dal mondo del lavoro e dai territori grazie a interazioni con soggetti
                     esterni (enti, aziende, artigiani, iniziative imprenditoriali). </p>
 
-                <h4 class="pt-4 pb-4">Edizione 2024 </h4>
+                <h4 class="pt-4 pb-4">Edizione 2024  &nbsp;👉<a href="/img/ws-24-programma.pdf" target="_blank"> scarica il calendario</a> 👈</h4>
+                
                 <div class="col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3">
                     <!-- WS-->
-                    <div class="grid grid-cols-8 workshop mb-2 border-b  border-black">
-                        <img class="pic hidden sm:block " alt="foto di Rodrigo Ramirez" src="/img/ws-24-ramirez.png" />
-                        <div class="col-span-6 p-4">
-                            <p><strong>Rodrigo Ramirez</strong></p>
-                            <p>Visual Information for Emergencies</p>
-                            
-                        </div>
-                        <div class="py-4 justify-self-end self-center">
-                            <a @click.prevent="myAccordion" class="more_btn" href="#"></a>
-                        </div>
-                        <!-- extra content -->
-                        <div class="col-span-8 col-start-1 extra_content">
-                           
-                            <p>Un workshop con cui esplorare e prototipare il design delle informazioni impiegando le icone del sistema Guemil per l'Emergenza.
-
-                            </p>
-                            <br/>
-                            <p class="italic">Rodrigo Ramírez ha conseguito un MA in Information Design presso l'Università di Reading nel Regno Unito, dopo aver studiato Design all'UCV di Valparaíso, Cile.
-                            È Professore Associato alla School of Design di UC Chile, dove insegna nel Master in Advanced Design MADA UC.  E' Design leader, del progetto Guemil–Icons for emergencies e Membro del Board di Design for Emergency Management (dnem.org).</p>
-                            <p>
-                            Rodrigo ha una estesa esperienza come Visting Professor e ha diretto progetti di Design dell'informazione per sistemi di informazioni, brands, organizzazioni pubbliche e iniziative editoriali. Si interessa, sia sul piano operativo che della ricerca, di Information Design e di Tipografia. Al momento sta seguendo un PhD in Comunicazione presso UC Cile.
-                            </p>
-                            
-                        
-
-                               
-                        </div>
-                    </div>
-                    
                     <div class="grid grid-cols-8 workshop mb-2  border-b border-black">
                         <img class="pic hidden sm:block " alt="foto di Viola Orgiano" src="/img/ws-24-orgiano.png" />
                         <div class="col-span-6 p-4">
@@ -159,6 +131,38 @@
                                 <div class="logo-wrapper grid grid-cols-2 items-baseline gap-x-1.5">
                                     <img class="logo" src="/img/gommamica.png"/> <img class="logo" src="/img/giochi-infiniti.png"/>
                                 </div>
+                        </div>
+                    </div>
+
+                    
+                    <h4 class="pt-4">Sessione di warmup</h4>
+
+                    <div class="grid grid-cols-8 workshop mb-2 border-b border-t border-black">
+                        <img class="pic hidden sm:block " alt="foto di Rodrigo Ramirez" src="/img/ws-24-ramirez.png" />
+                        <div class="col-span-6 p-4">
+                            <p><strong>Rodrigo Ramirez</strong></p>
+                            <p>Visual Information for Emergencies</p>
+                            
+                        </div>
+                        <div class="py-4 justify-self-end self-center">
+                            <a @click.prevent="myAccordion" class="more_btn" href="#"></a>
+                        </div>
+                        <!-- extra content -->
+                        <div class="col-span-8 col-start-1 extra_content">
+                           
+                            <p>Un workshop con cui esplorare e prototipare il design delle informazioni impiegando le icone del sistema Guemil per l'Emergenza.
+
+                            </p>
+                            <br/>
+                            <p class="italic">Rodrigo Ramírez ha conseguito un MA in Information Design presso l'Università di Reading nel Regno Unito, dopo aver studiato Design all'UCV di Valparaíso, Cile.
+                            È Professore Associato alla School of Design di UC Chile, dove insegna nel Master in Advanced Design MADA UC.  E' Design leader, del progetto Guemil–Icons for emergencies e Membro del Board di Design for Emergency Management (dnem.org).</p>
+                            <p>
+                            Rodrigo ha una estesa esperienza come Visting Professor e ha diretto progetti di Design dell'informazione per sistemi di informazioni, brands, organizzazioni pubbliche e iniziative editoriali. Si interessa, sia sul piano operativo che della ricerca, di Information Design e di Tipografia. Al momento sta seguendo un PhD in Comunicazione presso UC Cile.
+                            </p>
+                            
+                        
+
+                               
                         </div>
                     </div>
                    
