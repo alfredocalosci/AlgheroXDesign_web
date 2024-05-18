f191ab05674ed0c271779aa61ab83623fd1e5bba
Matteo Moretti <hello@matteomoretti.com>
2024-05-18 17:24:56 +0200

    admission page in progress

--- algheroXdesign/pages/ammissione.vue
+++ algheroXdesign/pages/ammissione.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto pds">
+    <div class="container mx-auto">
         <titolo titoloPagina="Guida ai test di ammissione"></titolo>
         <div class="grid grid-cols-12 gap-x-6 mb-12">
             <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3">
@@ -19,20 +19,23 @@
 
                 <h4 id="prova">La prova grafico-testuale</h4>
                 <p>
-                    Carta, penna, matite colorate: l’idea è quella di ridurre ai minimi
+                    Carta, penna, matite colorate: l'idea è quella di ridurre ai minimi
                     termini le possibilità espressive, affinché tutti gli studenti abbiano
-                    le stesse opportunità. Nella prova grafico-testuale, non ci si aspetta
-                    che i candidati sappiano disegnare, piuttosto che affrontino i temi
-                    proposti elaborando concetti originali, non scontati, da prospettive
-                    inusuali o personali. Nello specifico, sono tenuti in considerazione
-                    la chiarezza e coerenza nell’articolazione del ragionamento,
-                    l’efficacia dell’espressione grafica, la capacità di integrazione di
-                    pensiero concettuale e linguaggio visivo, ed infine l’originalità
-                    della proposta e dell’elaborazione.
+                    le stesse opportunità. <br />Non cerchiamo necessariamente il bel disegno – a disegnare si impara
+                    (anche
+                    qui, all'Università!), e poi ognuno ha un suo stile e un suo modo di rappresentare il mondo
+                    graficamente – ma vorremmo piuttosto che affrontiaste i temi proposti elaborando concetti originali,
+                    non scontati, da prospettive inusuali o personali. </p>
+                <p>Potete anche giocare con le lettere, con le
+                    parole, mescolandole a disegni e a segni. Ciò che vogliamo capire è come riuscite, …anche sotto un
+                    po di stress, a esprimervi con chiarezza e coerenza nell'articolazione di un ragionamento, a
+                    esprimere con efficacia grafica e non solo la vostra idea, anche integrando il pensiero concettuale
+                    e il linguaggio visivo. Poi ovviamente conta anche l'originalità della proposta e della vostra
+                    elaborazione.
                 </p>
                 <p>Sono
                     inoltre
-                    previsti tempi supplementari per studenti con disturbi specifici dell’apprendimento (+30%) o con
+                    previsti tempi supplementari per studenti con disturbi specifici dell'apprendimento (+30%) o con
                     disabilità (+50%).</p>
 
                 <h4 id="colloquio">Colloquio</h4>
@@ -64,7 +67,7 @@
 
                     È naturale sentirsi disorientati, soprattutto nei primi momenti: anche questo fa parte della
                     prova
-                    grafico-testuale. Niente panico, ci sono 4 ore di tempo per provare un’idea, metterla da parte,
+                    grafico-testuale. Niente panico, ci sono 4 ore di tempo per provare un'idea, metterla da parte,
                     provare nuove strade, ritornare sui propri passi, fino a quando non si è soddisfatti.
                 </p>
                 <h4 class="pt-10 pb-4">
@@ -77,7 +80,7 @@
                 <div class="grid grid-cols-2 gap-x-6 mb-12 border-b border-black">
                     <div class="bg-slate-100"><img src="/img/admission-a37.jpg" alt="" /></div>
                     <div>
-                        <p>La prova, relativa alla traccia A, denota un’ottima capacità di sintesi e
+                        <p>La prova, relativa alla traccia A, denota un'ottima capacità di sintesi e
                             concettualizzazione. Comunica in maniera chiara e sintetica le passioni e gli stimoli che
                             hanno portato a scegliere il corso di laurea in design. Ottima la padronanza del tratto.</p>
                     </div>
@@ -87,7 +90,7 @@
                     <div>
                         <p>La prova, relativa alla traccia A, visualizza le esperienze e le passioni che hanno motivato
                             la scelta di studiare design. Gli elementi rappresentati, supportati da brevi testi di
-                            contorno, rivelano la curiosità e la passione per l’arte, così come per il design.</p>
+                            contorno, rivelano la curiosità e la passione per l'arte, così come per il design.</p>
                     </div>
                 </div>
                 <div class="grid grid-cols-2 gap-x-6 mb-12 border-b border-black">
@@ -95,7 +98,7 @@
                     <div>
                         <p>La prova, relativa alla traccia A, visualizza le esperienze e le passioni che hanno motivato
                             la scelta di studiare design. Ogni riquadro sintetizza visivamente una passione
-                            dell’autrice, componendo il mosaico di esperienze che portano al mondo del design, scritto
+                            dell'autrice, componendo il mosaico di esperienze che portano al mondo del design, scritto
                             in primo piano.
 
                         </p>
@@ -115,6 +118,81 @@
 
             </div>
         </div>
+        <div class="grid grid-cols-12 gap-x-6 mb-12">
+            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3">
+                <p class="pb-4">
+                <h4> Una nota del presidente del corso di Laurea, Nicolò Ceccarelli</h4>
+
+                <p>Lo scorso settembre quasi 60 candidate/i sono state impegnate/i nelle prove per
+                    l'ammissione a Design qui ad Alghero. Prove (quella pratica grafico testuale, e il colloquio) con i
+                    risultati delle quali redigiamo la graduatoria delle ammesse/i.
+                </p>
+                <p>
+
+                    Siamo ben consci di come queste prove, e il percorso stesso di selezione, possa essere duro, anche
+                    frustrante, in particolar modo per chi poi non viene selezionato.
+                </p>
+                <p>
+                    Anche per questo motivo abbiamo configurato le prove per coinvolgere i partecipanti in una
+                    esperienza, in un momento magari complesso, difficile, che però sia coinvolgente e inclusivo.
+                    E quindi la prova pratica vuol'essere una simulazione, quasi un'antipasto di una condizione tipo
+                    della quotidianità di questo lavoro: domande e tracce imprevedibili, al limite dello spiazzante, che
+                    riproducono condizioni in cui designer immancabilmente si ritrova nella sua attività professionale.
+                </p>
+                <p>
+                    La capacità di reagire, di rispondere a questo tema imprevisto, con un guizzo, una trovata, è
+                    proprio la cosa che cerchiamo di verificare.
+                    Inutile nasconderlo: ad alcuni candidati e candidate questo momento risulta problematico: ma se
+                    vogliono prendere questa strada, questa condizione sarà per loro quasi quotidiana.
+                    Da un lato è allora bene aver chiara questa condizione da subito, e dall'altra non è mai tardi per
+                    iniziare a prepararsi aper questo genere di imprevisti.
+                </p>
+                <p>
+                    A partire da quest'anno abbiamo deciso di chiedere a un collega, un amico designer, di cimentarsi
+                    nella prova parallelamente a voi.
+                    Obiettivo è mostrare uno dei modi con cui risolvere il tema assegnato, è un assaggio di come, volta
+                    acquisiti gli elementi propri del mestiere, i designer e le designer mature può rispondere alle
+                    sfide, spesso davvero impreviste, del mestiere.
+                </p>
+                <p>
+                    Parallelamente alla prova dello scorso settembre nel complesso di Santa Chiara Alghero, <a
+                        href="https://www.ecu.edu.au/schools/arts-and-humanities/staff/profiles/design/associate-professor-stuart-medley"
+                        target="_blank">Stuart
+                        Medley </a>ha realizzato per noi – in condizioni del tutto analoga a quella dei candidate e
+                    delle
+                    candidate – lo stesso tema.
+                </p>
+                <h4>
+                    Non svegliare il can che dorme di Stuart Medley
+                </h4>
+                <div class="grid grid-cols-2 gap-x-6">
+                    <div class="bg-slate-100"><img src="/img/admission-stuart-1.png" alt="" /></div>
+                    <div class="bg-slate-100"><img src="/img/admission-stuart-2.png" alt="" /></div>
+                </div>
+                <p class="caption">Le due creazioni di Stuart Medley ispirate alla prova 'non svegliare il can che
+                    dorme'</p>
+                <p class="text-base text-left text-black">
+
+                    Professore Associato di Design alla Edith Cowan University, a Perth in Australia. Stuart Medley crea
+                    fumetti e storyboard a supporto della pianificazione di servizi e a sostegno di iniziative per
+                    l'occupazione e lo housing.
+                    La sua ricerca si focalizza sulla comunicazione intenzionale attraverso le immagini. Autore del
+                    libro The Picture in Design, Stuart ha guidato design workshops nel Regno Unito, Cina, Europa e
+                    Australia.
+                    È tra dei fondatori ed è presidente del Perth Comic Arts Festival, nonché Art Director di Hidden
+                    Shoal, etichetta discografica australiana.
+                    Lavira anche professionalmente come illustratore e visual designer e ha tra i suoi committenti
+                    Anglicare, Austal Ships e il governo dell'Australia occidentale.
+
+                </p>
+                </p>
+            </div>
+        </div>
+
+
+
+
+
     </div>
 
 
