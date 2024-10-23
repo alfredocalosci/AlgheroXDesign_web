8cded1f834231027bbaca47f1e2f04d005586dca
Matteo Moretti <hello@matteomoretti.com>
2024-10-23 11:19:19 +0200

    typo fix

--- algheroXdesign/components/saperne.vue
+++ algheroXdesign/components/saperne.vue
@@ -1,22 +1,26 @@
 <template>
     <div id="modulo" class="col-span-8 col-start-3 border-t border-black pt-12">
-        <p class="text-4xl px-6"><a href="https://www.architettura.aho.uniss.it/it/didattica/corso-di-laurea-design">Se vuoi iscriverti? Parti da qui!</a> Oppure scopri <NuxtLink to="/studiare-ad-alghero">perchè studiare ad Alghero</NuxtLink>, i <NuxtLink to="/docenti">docenti</NuxtLink> e le materie del <NuxtLink to="/piano-studi">piano di studi</NuxtLink>.</p>
+        <p class="text-4xl px-6"><a
+                href="https://www.architettura.aho.uniss.it/it/didattica/corso-di-laurea-design">Vuoi iscriverti? Parti
+                da qui!</a> Oppure scopri <NuxtLink to="/studiare-ad-alghero">perchè studiare ad Alghero</NuxtLink>, i
+            <NuxtLink to="/docenti">docenti</NuxtLink> e le materie del <NuxtLink to="/piano-studi">piano di studi
+            </NuxtLink>.</p>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
+    setup() {
         return {}
     }
 }
 </script>
 
 <style lang="scss" scoped>
-#modulo{
-        background-image: url("@/assets/img/axd_gray.svg");
+#modulo {
+    background-image: url("@/assets/img/axd_gray.svg");
     // background-color: #cccccc;
-  // height: 500px;
+    // height: 500px;
     background-position: center;
     background-repeat: no-repeat;
     background-size: 90% auto;
@@ -24,10 +28,10 @@ export default {
 
     padding-bottom: 3rem;
 
-        p{
-            letter-spacing: -0.05em;
-            line-height: 2.75rem;
-        }
-        
+    p {
+        letter-spacing: -0.05em;
+        line-height: 2.75rem;
     }
+
+}
 </style>
