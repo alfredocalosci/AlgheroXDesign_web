ad5be25e5993ce81bbde9dcc67bc1165d4b893d1
Matteo Moretti <hello@matteomoretti.com>
2025-05-20 09:00:12 +0200

    prova finale page added

---
+++ algheroXdesign/pages/prova-finale.vue
@@ -0,0 +1,288 @@
+<template>
+    <div class="container mx-auto" id="pds">
+        <titolo titoloPagina="Prova finale"></titolo>
+        <div class="grid grid-cols-12 gap-x-6 mb-12">
+
+
+            <div class="px-4 col-span-12 col-start-1 sm:px-0 lg:col-span-8 lg:col-start-3 ">
+
+
+                <h4 class="pt-8">Elaborato di fine carriera</h4>
+                <p>
+                    La prova finale ha come obiettivo la valutazione, da parte di una commissione di docenti del Corso
+                    di Laurea, della maturità acquisita dallo studente o dalla studentessa lungo il suo percorso
+                    formativo.
+                </p><br />
+                <p>
+                    La prova consiste nella presentazione orale di un <strong>elaborato di fine carriera</strong>:
+                    un <strong>Progetto (A)</strong> – a scelta – di un prodotto, di un servizio, di artefatti visuali,
+                    multimediali e
+                    interattivi, accompagnato dalla relazione di progetto;
+                    oppure, in alternativa, una <strong>Ricerca (B)</strong> teorica, storica o critica (nella forma di
+                    un elaborato
+                    scritto).
+                </p><br />
+                <p>
+                    Alternativamente, i candidati e le candidate potranno decidere di affrontare la prova finale
+                    presentando il lavoro condotto durante l’esperienza di tirocinio, attraverso un’approfondita
+                    discussione
+                    dell’esperienza svolta e tramite una <strong>Relazione di fine tirocinio (C)</strong>, che argomenti
+                    sia il loro
+                    specifico apporto a quanto presentato, sia i risultati formativi conseguiti nel periodo. Le
+                    documentazioni o relazioni scritte sono da presentarsi in formato cartaceo.
+                </p><br />
+                <p>
+                    In tutti i casi <strong>(A, B, C)</strong>la presentazione orale dovrà essere accompagnata dal
+                    <u>portfolio personale</u> ,
+                    illustrante una selezione significativa dei progetti realizzati nel corso degli studi.
+
+                </p>
+                <h4 class="pt-8">Relazione di progetto (A)</h4>
+                <p>
+                    Il progetto dovrà essere accompagnato da una relazione della lunghezza tra i 20 e i 25 mila
+                    caratteri, spazi e bibliografia inclusi, che descrive in modo approfondito le diverse fasi del
+                    percorso progettuale: la ricerca iniziale e l’individuazione del tema; la discussione di casi studio
+                    coerenti; gli elementi di ispirazione o referenze; fino allo sviluppo dell’idea e alla realizzazione
+                    prototipale, indicando possibili direzioni di sviluppo future. La relazione si chiude con una
+                    bibliografia di supporto.
+
+
+                </p>
+
+
+
+
+                <h4 class="pt-8">Relazione di ricerca (B)</h4>
+                <p>Le ricerche di tipo teorico, storico o critico – da condurre sotto la supervisione di un docente –
+                    devono essere presentate in forma di testi della lunghezza tra i 25 e i 35 mila caratteri, spazi e
+                    bibliografia inclusi. La relazione di ricerca, da consegnare in formato cartaceo, descrive in
+                    maniera critica il tema affrontato elaborando un ragionamento di tipo teorico, storico, supportato
+                    da una panoramica della letteratura esistente e/o di esperienze ad essa riconducibili; dei necessari
+                    riferimenti teorico/critici, oltre che da una bibliografia ragionata di supporto.
+
+                </p>
+
+
+
+                <h4 class="pt-8">Relazione di tirocinio (C)</h4>
+                <p>Consiste in un elaborato testuale, da consegnare in formato cartaceo, che descrive e ripercorre il
+                    percorso svolto nell’esperienza di tirocinio.
+                    L’elaborato, della lunghezza di almeno 15 mila caratteri spazi inclusi, dovrà presentare con
+                    chiarezza il contesto nel quale si è operato – anche discutendo esempi e casi studio relativi al
+                    campo in oggetto –, le attività in cui si è stati coinvolti, le competenze acquisite. Suo principale
+                    obiettivo è evidenziare il risultato formativo dell’esperienza svolta, in relazione agli strumenti e
+                    alle conoscenze sviluppate durante il Corso di laurea e dimostrare la crescita individuale che ha
+                    prodotto, anche in termini di consapevolezza.
+                </p>
+
+                <h4 class="pt-8">Portfolio personale</h4>
+                <p>
+                    Il portfolio è una componente fondamentale della prova finale e accompagna obbligatoriamente tutte
+                    le tipologie di prova finale (Progetto, Ricerca, Relazione di tirocinio). Consiste in una raccolta
+                    ragionata dei lavori più significativi realizzati durante il percorso di studi – ma anche esperienze
+                    realizzate parallelamente ad esso – utile a testimoniare il livello di maturazione progettuale,
+                    teorica e tecnica raggiunto.
+                    I progetti inclusi nel portfolio devono essere corredati da una breve descrizione che ne illustri il
+                    contesto, gli obiettivi, le fasi principali e i risultati ottenuti.
+
+
+                </p>
+                <h4 class="pt-8">Presentazione dell’elaborato di fine carriera</h4>
+                <p>
+                    La presentazione/discussione dell’elaborato di fine carriera si svolge in presenza di una
+                    commissione di docenti del Corso di Laurea, ha una durata di 15 minuti, più 5 per le domande e la
+                    discussione.
+                    Ha generalmente luogo appoggiandosi alla video proiezione di una presentazione appositamente
+                    realizzata.
+                    Nel caso A questa è accompagnata da materiali visivi, modelli e prototipazioni necessarie a
+                    permettere alla commissione la valutazione della qualità del progetto svolto. In occasione della
+                    discussione, sarà cura di candidati e candidate presentare almeno due copie cartacee delle relazioni
+                    di progetto, per permetterne la visione/valutazione alla commissione.
+                    Anche nei casi B e C sarà necessario presentare almeno due copie cartacee degli elaborati testuali
+                    prodotti.
+
+
+                </p>
+
+
+                <h4 class="pt-8">Considerazioni generali e criteri di valutazione</h4>
+                <p>
+                    Arricchito da materiali visivi e riflessioni personali, l’elaborato di fine carriera non è solo una
+                    testimonianza del percorso formativo svolto, ma anche un’occasione per acquisire maggiore
+                    consapevolezza sul proprio ruolo di progettisti e progettiste.
+
+                    Ai/alle candidati/e è richiesto di dimostrare una capacità analitica e di sintesi, secondo un
+                    modello ispirato a un approccio multidimensionale (controllando dunque sia la scala d’insieme, in
+                    termini di processo, sia il dettaglio, ovvero proponendo soluzioni puntuali e specifiche).
+                    I/le candidati/e dovranno inoltre dar prova di aver acquisito piena familiarità con il tema
+                    affrontato, anche dimostrando la necessaria abilità comunicativa e la scelta di appropriate modalità
+                    e linguaggi per la sua trasmissione.
+
+                    I principali elementi oggetto di valutazione saranno: il raggiungimento di una capacità autonoma di
+                    controllo metodologico; la complessiva qualità degli esiti finali, nonché la loro coerenza;
+                    l’acquisizione di una piena consapevolezza del lavoro – progetto, ricerca o esperienza di tirocinio
+                    – svolto.
+
+                    Anche il portfolio personale – componente fondamentale che accompagna obbligatoriamente tutte le
+                    tipologie di prova finale – concorrerà in maniera complementare alla valutazione complessiva.
+                    In particolare, a candidati/e è richiesto di delineare, argomentandole, le esperienze che ritengono
+                    più significative per la loro maturazione formativa, anche in relazione ai temi della loro prova
+                    finale.
+                </p><br />
+                <p>
+                    <a href="#">Traccia per la relazione di tirocinio</a>
+
+                </p>
+
+
+
+            </div>
+
+
+
+
+
+
+        </div>
+        <saperne></saperne>
+    </div>
+</template>
+
+<script lang="ts">
+
+// carousel
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+
+export default defineComponent({
+    name: 'Basic',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+    methods: {
+        myAccordion(e) {
+            let p = e.target.parentNode.parentNode;
+            let t = p.getElementsByClassName('extra_content')
+
+            e.target.classList.toggle("open");
+            t[0].classList.toggle("open");
+
+            //  console.log(e.target.parentNode.parentNode)
+            // console.log("clicked")
+        }
+    }
+})
+
+</script>
+
+
+<script setup lang="ts">
+definePageMeta({ layout: 'default' })
+
+useHead({
+    title: 'AlgheroXDesign - Laboratori di sintesi finale',
+    meta: [
+        { name: 'description', content: 'AlgheroXDesign forma giovani designer di prodotto, della comunicazione e dei servizi per ricominciare a immaginare, comunicare e innovare la società, a partire dalla Sardegna.' },
+        { name: 'og:description', content: 'AlgheroXDesign forma giovani designer di prodotto, della comunicazione e dei servizi per ricominciare a immaginare, comunicare e innovare la società, a partire dalla Sardegna.' },
+        { name: 'twitter:description', content: 'AlgheroXDesign forma giovani designer di prodotto, della comunicazione e dei servizi per ricominciare a immaginare, comunicare e innovare la società, a partire dalla Sardegna.' },
+        { name: 'title', content: 'AlgheroXDesign, Laurea triennale in Design: studiare design ad Alghero' },
+        { name: 'keywords', content: 'Design, sardegna, università, grafica, digital, prodotto, servizio, docenti' },
+        { name: 'og:title', content: 'AlgheroXDesign, Laurea triennale in Design: studiare design ad Alghero' },
+        { name: 'twitter:title', content: 'AlgheroXDesign, Laurea triennale in Design: studiare design ad Alghero' }
+    ],
+    link: [
+        { rel: 'canonical', href: 'http://algheroxdesign.it/laboratori-di-sintesi-finale' }
+    ]
+})
+</script>
+
+
+<style lang="scss" scoped>
+.workshop h4 {
+    line-height: 100%;
+}
+
+.workshop p {
+    font-size: 20px;
+}
+
+.workshop p.small {
+    font-size: 14px;
+    margin-bottom: 4px;
+    font-style: italic;
+}
+
+.info {
+    padding-bottom: 50px;
+
+    p {
+        max-width: 550px;
+    }
+
+    ul {
+        margin-left: 0px;
+        list-style-type: disc;
+
+        li {
+            padding-bottom: 2px;
+        }
+    }
+}
+
+.logo-wrapper {
+    max-width: 320px;
+
+    img.logo {
+        width: 150px;
+    }
+}
+
+.more_btn {
+    transition: background-color 0.5s, color 0.5s;
+    background-color: #EEEAEC;
+    font-size: 2rem;
+    text-align: center;
+    text-decoration: none;
+    width: 3rem;
+    height: 3rem;
+    display: block;
+    border-radius: 1.5rem;
+    font-weight: 300;
+    text-shadow: none;
+    border-color: transparent;
+
+    &::before {
+        content: "+";
+    }
+
+    &:hover {
+        background-color: #333;
+        color: #fff;
+    }
+
+    &.open {
+        background-color: #000;
+        color: #fff;
+
+        &::before {
+            content: "-";
+        }
+    }
+}
+
+.extra_content {
+    transition: height 0.5s;
+
+    height: 0px;
+    overflow: hidden;
+
+    &.open {
+        height: auto;
+        padding: 0.75rem 0;
+    }
+}
+</style>
